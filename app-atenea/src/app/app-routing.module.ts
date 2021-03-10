7b7cb49f7552b3229822d0b4bc1591e38e5a303c
Maria Jose Perez <mariperez259@gmail.com>
2021-03-10 11:03:53 -0400

    update appcomponent

--- app-atenea/src/app/app-routing.module.ts
+++ app-atenea/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', pathMatch: 'full', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
